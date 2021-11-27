d6ac355ff0831297109c1afba83969ea91ccc719
Rennan Rezende <rennan765@gmail.com>
2021-11-27 16:22:12 -0300

    Domain.Data

---
+++ 4oito6.Demonstration.Person.Domain.Data/Transaction/IPersonUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using _4oito6.Demonstration.Domain.Data.Repositories;
+using _4oito6.Demonstration.Person.Domain.Data.Repositories;
+
+namespace _4oito6.Demonstration.Person.Domain.Data.Transaction
+{
+    public interface IPersonUnitOfWork : IPersonRepositoryRoot, IBaseRepositoryRoot
+    {
+    }
+}
