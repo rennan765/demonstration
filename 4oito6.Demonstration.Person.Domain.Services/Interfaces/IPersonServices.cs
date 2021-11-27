7d995ad74acaec148a920f05d5ac9221f206b050
Rennan Rezende <rennan765@gmail.com>
2021-11-27 20:55:31 -0300

    Domain Services

---
+++ 4oito6.Demonstration.Person.Domain.Services/Interfaces/IPersonServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _4oito6.Demonstration.Person.Domain.Services.Interfaces
+{
+    using _4oito6.Demonstration.Domain.Model.Entities;
+
+    public interface IPersonServices : IDisposable
+    {
+        Task<Person> GetByIdAsync(int id);
+
+        Task<Person> CreateAsync(Person person);
+
+        Task<Person> UpdateAsync(Person person);
+    }
+}
