978d73298c2710bb9c8b6de4ac9c280f67481065
Rennan Rezende <rennan765@gmail.com>
2021-11-29 10:44:22 -0300

    DynamoConnection

---
+++ 4oito6.Demonstration.Data/Connection/DynamoConnection.cs
@@ -0,0 +1,115 @@
+﻿using _4oito6.Demonstration.Commons;
+using _4oito6.Demonstration.Data.Model.Dynamo;
+using _4oito6.Demonstration.Data.Model.Dynamo.Base;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _4oito6.Demonstration.Data.Connection
+{
+    public class DynamoConnection<T> : DisposableObject, IDynamoConnection<T> where T : DynamoEntityDto, new()
+    {
+        private readonly IAmazonDynamoDB _conn;
+        private readonly Table _table;
+
+        public DynamoConnection(IAmazonDynamoDB conn) : base(new IDisposable[] { conn })
+        {
+            _conn = conn ?? throw new ArgumentNullException(nameof(conn));
+            _table = Table.LoadTable(_conn, new T().TableName);
+        }
+
+        public Task<Document> CreateAsync(Document document)
+        {
+            if (document is null)
+            {
+                throw new ArgumentNullException(nameof(document));
+            }
+
+            return _table.PutItemAsync(document);
+        }
+
+        public async Task<T> CreateAsync(T document)
+        {
+            if (document is null)
+            {
+                throw new ArgumentNullException(nameof(document));
+            }
+
+            var result = await CreateAsync(document.ToDocument()).ConfigureAwait(false);
+
+            return result?.ToEntity<T>();
+        }
+
+        public Task DeleteAsync(string partitionKey)
+        {
+            return _table.DeleteItemAsync(partitionKey, new DeleteItemOperationConfig());
+        }
+
+        public Task DeleteAsync(Document document)
+        {
+            return _table.DeleteItemAsync(document, new DeleteItemOperationConfig());
+        }
+
+        public Task DeleteAsync(T document)
+        {
+            return DeleteAsync(document.ToDocument());
+        }
+
+        public async Task<IEnumerable<T>> FindAllAsync()
+        {
+            return (await _table.Scan(default(Expression)).GetRemainingAsync().ConfigureAwait(false))
+                .Select(document => document.ToEntity<T>())
+                .ToList();
+        }
+
+        public async Task<IEnumerable<T>> FindByDateTicksAsync(string dateProperty, long startTicks, long endTicks)
+        {
+            var scanFilter = new ScanFilter();
+            var filterValues = new DynamoDBEntry[] { startTicks, endTicks };
+
+            scanFilter.AddCondition(dateProperty, ScanOperator.Between, filterValues);
+            var search = _table.Scan(scanFilter);
+
+            var documents = new List<Document>();
+            do
+            {
+                documents.AddRange(await search.GetNextSetAsync().ConfigureAwait(false));
+            }
+            while (search.IsDone);
+
+            return documents
+                .Select(document => document.ToEntity<T>())
+                .ToList();
+        }
+
+        public Task<Document> UpdateAsync(Document document)
+        {
+            if (document is null)
+            {
+                throw new ArgumentNullException(nameof(document));
+            }
+
+            var config = new UpdateItemOperationConfig
+            {
+                ReturnValues = ReturnValues.AllNewAttributes
+            };
+
+            return _table.UpdateItemAsync(document, config);
+        }
+
+        public async Task<T> UpdateAsync(T document)
+        {
+            if (document is null)
+            {
+                throw new ArgumentNullException(nameof(document));
+            }
+
+            var result = await UpdateAsync(document.ToDocument()).ConfigureAwait(false);
+
+            return result?.ToEntity<T>();
+        }
+    }
+}
---
+++ 4oito6.Demonstration.Data/Model/Dynamo/DataMapper.cs
@@ -0,0 +1,65 @@
+﻿using _4oito6.Demonstration.Data.Model.Dynamo.Base;
+using Amazon.DynamoDBv2.DocumentModel;
+using Linq2DynamoDb.DataContext.Utils;
+using System;
+
+namespace _4oito6.Demonstration.Data.Model.Dynamo
+{
+    public static class DataMapper
+    {
+        public static Document ToDocument<T>(this T document) where T : DynamoEntityDto
+        {
+            var doc = new Document();
+            var properties = document.GetType().GetProperties();
+
+            foreach (var property in properties)
+            {
+                if (property.Name == nameof(document.TableName) || property.Name == nameof(document.Namespace))
+                {
+                    continue;
+                }
+
+                var value = property.GetValue(document, null);
+                var type = property.PropertyType;
+                doc[property.Name] = value?.ToDynamoDbEntry(type);
+            }
+
+            return doc;
+        }
+
+        private static T ToDataObject<T>(this Document document) where T : DynamoEntityDto
+        {
+            if (document is null)
+            {
+                return default;
+            }
+
+            var entity = (T)Activator.CreateInstance(typeof(T));
+            var properties = typeof(T).GetProperties();
+
+            foreach (var property in properties)
+            {
+                if (!document.ContainsKey(property.Name))
+                {
+                    continue;
+                }
+
+                var value = document[property.Name];
+
+                if (property.PropertyType.Equals(typeof(DateTime)))
+                {
+                    property.SetValue(entity, value.ToObject(property.PropertyType));
+                }
+
+                property.SetValue(entity, value.ToObject(property.PropertyType));
+            }
+
+            return entity;
+        }
+
+        public static T ToEntity<T>(this Document document) where T : DynamoEntityDto
+        {
+            return document.ToDataObject<T>();
+        }
+    }
+}
