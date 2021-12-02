270b336af87f5b1d0e0ea10a0ef934da5c69acdc
Rennan Rezende <rennan765@gmail.com>
2021-12-02 17:30:27 -0300

    Domain.Data

---
+++ Contact/4oito6.Demonstration.Contact.Domain.Data/Repositories/IPersonRepository.cs
@@ -0,0 +1,13 @@
+﻿using _4oito6.Demonstration.Domain.Model.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace _4oito6.Demonstration.Contact.Domain.Data.Repositories
+{
+    public interface IPersonRepository : IDisposable
+    {
+        Task<Person> GetByIdAsync(int id);
+
+        Task UpdateContactInformationAsync(Person person);
+    }
+}
---
+++ Contact/4oito6.Demonstration.Contact.Domain.Data/Transaction/IContactUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using _4oito6.Demonstration.Contact.Domain.Data.Repositories;
+using _4oito6.Demonstration.Domain.Data.Transaction;
+
+namespace _4oito6.Demonstration.Contact.Domain.Data.Transaction
+{
+    public interface IContactUnitOfWork : IBaseUnitOfWork, IContactRepositoryRoot
+    {
+    }
+}
